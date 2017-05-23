ac64c78c32e608565022fc6287e07f2aff685004
Gene Blanchard <gblanchard4@gmail.com>
2017-05-22 23:09:45 -0500

    filtering on checkboxes

--- shinyapp/server.R
+++ shinyapp/server.R
@@ -8,7 +8,12 @@ data$lng <- do.call("rbind", lapply(data$location.coordinates, "[[",1))
 data$lat <- do.call("rbind", lapply(data$location.coordinates, "[[",2))
 
 # Define server logic required to draw a leaflet map
-shinyServer(function(input, output) {
+shinyServer(function(input, output, session) {
+
+  # Data reacts to input
+  filteredData <- reactive({
+    subset(data, license_type %in% input$type)
+  })
 
   output$map <- renderLeaflet({
     leaflet() %>%
@@ -20,9 +25,16 @@ shinyServer(function(input, output) {
       lng = -90.08385663199994,
       lat = 29.980464294000058,
       zoom = 11
-    ) %>%
+    )
+  })
+
+
+  observe({
+    observeData <- filteredData()
+    leafletProxy("map") %>%
+    clearMarkers() %>%
     addMarkers(
-      data = data,
+      data = observeData,
       lng = ~lng,
       lat = ~lat,
       popup = ~address)
