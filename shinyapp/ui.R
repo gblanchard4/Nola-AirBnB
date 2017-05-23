3d06f32a10ffa45775e5d6ed16b6ee33607c6e79
Gene Blanchard <gblanchard4@gmail.com>
2017-05-22 22:40:14 -0500

    checkboxes

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -7,8 +7,17 @@ shinyUI(fluidPage(
   # Application title
   titlePanel("New Orleans Short Term Rentals"),
 
-    # Show a plot of the generated distribution
+  sidebarLayout(
+    sidebarPanel(
+      checkboxGroupInput(
+        inputId = "type",
+        label =  "License Type",
+        choices = c("Temporary STR", "Commercial STR", "Accessory STR"),
+        selected = c("Temporary STR", "Commercial STR", "Accessory STR"))
+    ),
+
     mainPanel(
       leafletOutput("map")
     )
+  )
 ))
